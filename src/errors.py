4fb89927737c76aa820ea293eb91bbee192ae8dc
Peter Hansen <mail@peterhansen.io>
2024-05-24 14:45:49 +0000

    Don't use Python Classes for Errors

--- src/errors.py
+++ src/errors.py
@@ -1,19 +1,17 @@
 """Errors (not Exceptions)"""
-
-from typing import Protocol, runtime_checkable
+from __future__ import annotations
+from typing import TypedDict
 
 NO_ERROR_T = type('NO_ERROR', (), {})
 NO_ERROR = NO_ERROR_T()
 
-@runtime_checkable
-class Error(Protocol):
-  """A Error"""
+class Error(TypedDict):
+  """An Error"""
 
   kind: str
   """The Kind of Error"""
   message: str
   """A Human Readable description about the Error that is helpful"""
 
-  def __str__(self) -> str:
-    """Pretty Print the Error for logging"""
-    return f"{type(self).__name__}({self.kind}): {self.message}"
+  @staticmethod
+  def render(error: Error) -> str: return f"{error['kind']}: {error['message']}"
