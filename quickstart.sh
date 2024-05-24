c1ba4b8ad4cdefaebbebadcfdb0c454e8851c8c2
Peter Hansen <mail@peterhansen.io>
2024-05-24 00:21:07 +0000

    Add Testing Example

--- quickstart.sh
+++ quickstart.sh
@@ -6,8 +6,11 @@ log() { printf -- "%s\n" "$*" >&2; }
 
 : "${CI_PROJECT_DIR:?Missing CI_PROJECT_DIR}"
 command -v jq >/dev/null || { log "jq is required"; exit 1; }
+
+### Build a Package
+
 (
-  log 'Building Package'
+  log 'Building the Package'
   cd "${CI_PROJECT_DIR}" || exit 1
   source "${CI_PROJECT_DIR}/.venv/bin/activate"
   rm -rf "${CI_PROJECT_DIR}/.cache/build" &>/dev/null || true
@@ -18,4 +21,13 @@ command -v jq >/dev/null || { log "jq is required"; exit 1; }
       --config="${CI_PROJECT_DIR}/config/stable/all.yaml" \
   | jq
 )
+
+### Test a Utils Package
+(
+  log 'Testing the Package'
+  cd "${CI_PROJECT_DIR}" || exit 1
+  bash "${CI_PROJECT_DIR}/test/entrypoint.sh" \
+    "config=${CI_PROJECT_DIR}/config/stable/all.yaml"
+)
+
 log 'fin'
